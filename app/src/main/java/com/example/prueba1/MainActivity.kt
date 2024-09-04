1c8db64f4981f8afb8a29d3db96e86b6a2bca3b1
yahonxz <jorge.sandova.c11@gmail.com>
2024-09-04 07:50:29 -0500

    Hizimos el ejemplo de como se usan los modificadores para cambiar el estilo de los elementos

--- app/src/main/java/com/example/prueba1/MainActivity.kt
+++ app/src/main/java/com/example/prueba1/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.prueba1.ui.theme.Prueba1Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
         //enableEdgeToEdge()
         setContent {
             //Layouts
-            Column {
+           /* Column {
                 //Los pone en filas
                 Text(text = "First Row")
                 Text(text = "Second Row")
@@ -43,6 +53,20 @@ class MainActivity : ComponentActivity() {
                 }
 
                 Greeting(name = "World!")
+            }*/
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment= androidx.compose.ui.Alignment.CenterHorizontally
+                
+                
+            ){
+                Text(text = "simple text")
+                ModifierExample2()
+                ModifierExample2()
+                ModifierExample3()
+
+
             }
         }
     }
@@ -60,6 +84,51 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Prueba1Theme {
-        Greeting("Jorge")
+        Greeting("Caro")
     }
+}
+@Preview(showBackground = true)
+@Composable
+fun  ModifierExample2(){
+    
+    Column (
+        modifier = Modifier
+            .padding(24.dp)
+            .fillMaxWidth()
+            .clickable(onClick = { clickAction() })
+        
+    ){
+        Text(text = "hello world")
+    }
+
+     
+}
+
+fun clickAction() {
+    println("Colum clicked")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun  ModifierExample3(){
+
+    Column (
+     modifier = Modifier
+         .fillMaxHeight()
+         .padding(16.dp)
+         .background(Color.Blue)
+         .border(width = 2.dp, color = Color.Green)
+         .width(200.dp),
+
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ){
+        Text(text = "item 1")
+        Text(text = "item 2")
+        Text(text = "item 3")
+        Text(text = "item 4")
+        Text(text = "item 5")
+    }
+
+
 }
