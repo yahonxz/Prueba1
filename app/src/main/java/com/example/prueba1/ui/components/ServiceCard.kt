59d6cfb37fe0ade8a6747121171dbc9d9c5f6d93
yahonxz <jorge.sandova.c11@gmail.com>
2024-11-27 22:40:06 -0800

    Feat: :sparkles: ServiceDetailCard was createsd

--- app/src/main/java/com/example/prueba1/ui/components/ServiceCard.kt
+++ app/src/main/java/com/example/prueba1/ui/components/ServiceCard.kt
@@ -4,23 +4,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Share
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,7 +24,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -37,11 +31,11 @@ import com.example.prueba1.R
 
 @Composable
 fun ServiceCard(
-    id : Int,
+    id:Int,
     name: String,
-    username: String,
-    imageURL: String?,
-    onButtonClick: () -> Unit
+    username:String,
+    imageURL:String?,
+    onButtonClick:()->Unit
 ){
     Card(
         modifier = Modifier
@@ -52,15 +46,15 @@ fun ServiceCard(
             contentColor = Color.White
         )
     ) {
-        Row() {
+        Row(
+        ) {
             AsyncImage(
                 modifier = Modifier
-                    .width(80.dp)
-                    .height(80.dp)
-                    .padding(8.dp),
-                model =imageURL,
-                error = painterResource(id = R.drawable.ic_launcher_foreground),
-                contentDescription = "ServiceLogo",
+                    .size(80.dp)
+                    .padding(10.dp),
+                model = imageURL,
+                error = painterResource(R.drawable.ic_launcher_background),
+                contentDescription = "Service Logo",
                 contentScale = ContentScale.FillBounds
             )
             Column(){
@@ -68,14 +62,14 @@ fun ServiceCard(
                     text = name,
                     fontSize = 18.sp,
                     fontWeight = FontWeight.Bold,
-                    color = Color.White,
-                    modifier = Modifier.padding(2.dp,8.dp,0.dp,0.dp)
+                    modifier = Modifier
+                        .padding(2.dp, 8.dp, 0.dp, 0.dp)
                 )
                 Text(
-                    text =username,
+                    text = username,
                     fontSize = 15.sp,
-                    color = Color.White,
-                    modifier = Modifier.padding(4.dp,0.dp)
+                    modifier = Modifier
+                        .padding(4.dp, 0.dp)
                 )
             }
             Row(
@@ -84,17 +78,19 @@ fun ServiceCard(
                     .height(80.dp),
                 horizontalArrangement = Arrangement.End,
                 verticalAlignment = Alignment.CenterVertically
-            ) {
+            ){
                 IconButton(
-                    modifier = Modifier.padding(20.dp,0.dp,0.dp,0.dp),
-                    onClick = { onButtonClick() }
-                ) {
+                    modifier = Modifier.padding(20.dp, 0.dp, 0.dp,0.dp),
+                    onClick = { onButtonClick() }){
                     Icon(
                         imageVector = Icons.Filled.MoreVert,
-                        contentDescription = "Service Details"
+                        contentDescription = "Service details"
                     )
+
                 }
+
             }
         }
     }
+
 }
